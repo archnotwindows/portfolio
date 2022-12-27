b242408d361222408a6eb1e2a7fb568bab8732ba
archnotwindows <108471111+archnotwindows@users.noreply.github.com>
2022-12-27 13:21:32 +0100

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+
+    document.body.classList.toggle("dark-theme");
+    if( document.body.classList.contains("dark-theme")){
+        icon.src ="files/sun.png";
+    }
+    else{
+
+        icon.src = "files/moon.png";
+    }
+}
+
